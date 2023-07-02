4f2ce5698051cdb587fc6cf2ad1ca7f1b79b8d0f
Bender <47286057+slavman123@users.noreply.github.com>
2023-07-01 20:11:19 -0700

    Update promptgenarator.py

--- promptgenarator.py
+++ promptgenarator.py
@@ -1,44 +1,47 @@
 import fileinput
+
 homescreen = True
-mainmenumess = "Go back to main menu?"
-while homescreen == True:
-    print('''/
+mainmenumess = "Go back to the main menu?"
 
+while homescreen:
+    print('''
+         ██████╗ ██████╗ ██████╗ ████████╗         ██╗██████╗ 
+        ██╔════╝██╔════╝ ██╔══██╗╚══██╔══╝         ██║██╔══██╗
+        ██║     ██║  ███╗██████╔╝   ██║            ██║██████╔╝
+        ██║     ██║   ██║██╔═══╝    ██║       ██   ██║██╔══██╗
+        ╚██████╗╚██████╔╝██║        ██║       ╚█████╔╝██████╔╝
+         ╚═════╝ ╚═════╝ ╚═╝        ╚═╝        ╚════╝ ╚═════╝ 
 
- ██████╗ ██████╗ ██████╗ ████████╗         ██╗██████╗ 
-██╔════╝██╔════╝ ██╔══██╗╚══██╔══╝         ██║██╔══██╗
-██║     ██║  ███╗██████╔╝   ██║            ██║██████╔╝
-██║     ██║   ██║██╔═══╝    ██║       ██   ██║██╔══██╗
-╚██████╗╚██████╔╝██║        ██║       ╚█████╔╝██████╔╝
- ╚═════╝ ╚═════╝ ╚═╝        ╚═╝        ╚════╝ ╚═════╝ 
-                                                       
-''')
-    print("Select Your Prompt:  "
-                   "1.Dev Mode  "
-                   "2.Aim Prompt "
-                   "3.AntiGPT    ")
+    ''')
+    print("Select Your Prompt: "
+          "1. Dev Mode "
+          "2. Aim Prompt "
+          "3. AntiGPT")
     userinput = input("Choose Prompt: ")
     if userinput == "1":
-        homescreen == False
+        # Dev Mode Prompt
+        homescreen = False
         devmodetextopen = open("devmode.txt", "r")
         file_contents = devmodetextopen.read()
         print(file_contents)
-        print("Do you want to paraphrase Dev Prompt?")
-        yesnoinput = input("Y/N:")
-        if yesnoinput == "n":
+        print("Do you want to paraphrase the Dev Prompt?")
+        yesnoinput = input("Y/N: ")
+
+        if yesnoinput.lower() == "n":
             print(mainmenumess)
-            homecreeninput = input("Y/N:")
-            if homecreeninput == "n":
+            homescreeninput = input("Y/N: ")
+            if homescreeninput.lower() == "n":
                 homescreen = False
-            homescreen == True
+            else:
+                homescreen = True
             devmodetextopen.close()
 
-        if yesnoinput == "y":
+        elif yesnoinput.lower() == "y":
             word_replacements = {
                 "created": "made",
                 "OpenAI": "your creators",
                 "Developer": "creator",
-                "From": "as off",
+                "From": "as of",
                 "normal": "regular",
                 "virtual machine": "VM",
                 "If": "while",
@@ -50,50 +53,60 @@ while homescreen == True:
             print(file_contents)
             devmodetextopen.close()
             print(mainmenumess)
-            homecreeninput = input("  Y/N:")
-            if homecreeninput == "n":
+            homescreeninput = input("Y/N: ")
+            if homescreeninput.lower() == "n":
                 homescreen = False
+            else:
+                homescreen = True
+
 
     elif userinput == "2":
+        # Aim Prompt
         homescreen = False
-        aimpromptopen = open("aimprompt.txt", "r")
-        file_contents = aimpromptopen.read()
-        print(file_contents)
-        print("Do you want to paraphrase Aim prompt ?")
-        yesnoinput6 = input("Y/N:")
-        if yesnoinput6 == "n":
-            homescreen = True
-            aimpromptopen.close()
-        elif yesnoinput6 == "y":
-            word_replacements = {
-                "hypothetical": "theoretical",
-                "AIM": "TRA",
-                "response": "reply",
-                "anything": "all",
-                "also":"too",
-                "inteligent":"smart"
-
-
-            }
-            for word, replacement in word_replacements.items():
-                file_contents = file_contents.replace(word, replacement)
+        while True:
+            aimpromptopen = open("aimprompt.txt", "r")
+            file_contents = aimpromptopen.read()
             print(file_contents)
-            aimpromptopen.close()
-                homecreeninput = input("Y/N:")
-                if homecreeninput == "y":
-                    homescreen = True
-            
+            print("Do you want to paraphrase the Aim prompt?")
+            yesnoinput6 = input("Y/N: ")
+
+            if yesnoinput6.lower() == "n":
+                aimpromptopen.close()
+                break
+            elif yesnoinput6.lower() == "y":
+                word_replacements = {
+                    "hypothetical": "theoretical",
+                    "AIM": "TRA",
+                    "response": "reply",
+                    "anything": "all",
+                    "also": "too",
+                    "intelligent": "smart"
+                }
+                for word, replacement in word_replacements.items():
+                    file_contents = file_contents.replace(word, replacement)
+                print(file_contents)
+                aimpromptopen.close()
+
+        print(mainmenumess)
+        homescreeninput = input("Y/N: ")
+        if homescreeninput.lower() == "n":
+            homescreen = False
+        elif homescreeninput.lower() == "y":
+            homescreen = True
+
     elif userinput == "3":
+        # AntiGPT Prompt
         homescreen = False
         AntiGPTprompt = open("AntiGPT.txt", "r")
         file_contents = AntiGPTprompt.read()
         print(file_contents)
-        print("Do you want to paraphrase AntiGPT prompt?")
-        yesnoinput3 = input("Y/N:")
-        if yesnoinput3 == "n":
+        print("Do you want to paraphrase the AntiGPT prompt?")
+        yesnoinput3 = input("Y/N: ")
+
+        if yesnoinput3.lower() == "n":
             homescreen = True
             AntiGPTprompt.close()
-        elif yesnoinput3 == "y":
+        elif yesnoinput3.lower() == "y":
             word_replacements = {
                 "prior": "past",
                 "Remain": "stay",
@@ -104,8 +117,6 @@ while homescreen == True:
                 file_contents = file_contents.replace(word, replacement)
             print(file_contents)
             AntiGPTprompt.close()
-            homescreeninput == input("Y/N"):
-            if homecreen == "y"
+            homescreeninput = input("Go Back to Main Menu?: ")
+            if homescreeninput.lower() == "y":
                 homescreen = True
-
-            
